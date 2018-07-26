5705f593e93709e686c30de7128960156be90357
Alejandra Alarcón <alalarco@dcc.uchile.cl>
2018-07-25 22:23:43 -0400

    Now it has a reason to be!

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Test Bot for Telegram
 #### English
 
-This is a **Telegram** test bot I'm making while I try to figure out what I want to do.
+This is a **Telegram** bot that shows World of Warcraft token price in a specific region indicated by the user.
 I use [Telepot](https://github.com/nickoala/telepot), a Python framework for Telegram Bot API created by [nickoala](https://github.com/nickoala).
 
 #### Español
 
-Este es un bot de prueba de **Telegram** que estoy haciendo mientras trato de ver lo que quiero hacer.
+Este es un bot de  **Telegram** que muestra el precio de la ficha de World of Warcraft en la región especificada por el usuario.
 Uso [Telepot](https://github.com/nickoala/telepot), un framework de Python para la API de bots de Telegram creado por [nickoala](https://github.com/nickoala).
--- testbot.py
+++ testbot.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+import json
 import telepot
 import time
 from telepot.loop import MessageLoop
+import urllib.request
+from config import *
 
-bot_token = '406091045:AAE-02I5NEO6INPjbyYqXB4fZjASaIX5Evo'
 bot = telepot.Bot(bot_token)
 
 
@@ -13,11 +15,18 @@ def handle(msg):
     chat_id = msg['chat']['id']  # guarda id del chat del que se recibe el mensaje
     mensaje = msg['text']  # texto del mensaje
     # bot.sendMessage(chat_id, "que tal") #bot envia un mensaje al chat por el que recibe el mensaje
-    a = mensaje.split(' ', 1)  # separa el mensaje en 2
+    a = mensaje.split(' ')  # separa el mensaje en 2
     comando = a[0]
     resto = a[1]  # el resto del mensaje
-    if comando.startswith('/echo'):
-        bot.sendMessage(chat_id, resto)
+    if comando.startswith('/region'):
+        bot.sendMessage(chat_id, token_price(resto))
+
+#Entrega el precio del token en la region indicada
+def token_price(region):
+    lonk = "https://data.wowtoken.info/wowtoken.json"
+    with urllib.request.urlopen(lonk) as url:
+        data = json.loads(url.read())
+        return data['update'][region]['raw']['buy']
 
 
 MessageLoop(bot, handle).run_as_thread()
